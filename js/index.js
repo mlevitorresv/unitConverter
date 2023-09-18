4c96b2621256828aa14aec4f677a5a8d505895d6
mlevitorresv <levitorres66@gmail.com>
2023-09-18 16:55:12 +0200

    conversor v.1

--- js/index.js
+++ js/index.js
@@ -1,55 +1,171 @@
-var result;
-const inputResult = document.getElementById("inputResult");
-
-function changeUnit(value){
-    switch(value.value){
+function changeUnit(){
+    const inputUnit = document.getElementById("unit");
+    switch(getSelected()){
         case "km-miles":
-            document.getElementById("unit").value="km";
+            inputUnit.value="km";
+            inputResultUnit.value= "miles";
         break;
         case "miles-km":
-            document.getElementById("unit").value="miles";
+            inputUnit.value="miles";
+            inputResultUnit.value= "km";
         break;
         case "foot-m":
-            document.getElementById("unit").value="foot";
+            inputUnit.value="foot";
+            inputResultUnit.value= "m";
         break;
         case "m-foot":
-            document.getElementById("unit").value="m";
+            inputUnit.value="m";
+            inputResultUnit.value= "foot";
         break;
         case "cm-inch":
-            document.getElementById("unit").value="cm";
+            inputUnit.value="cm";
+            inputResultUnit.value= "inch";
         break;
         case "inch-cm":
-            document.getElementById("unit").value="inch";
+            inputUnit.value="inch";
+            inputResultUnit.value= "cm";
+        break;
+    }
+    convert();
+}
+
+
+function convert(){
+    var result;
+    const inputResult = document.getElementById("inputResult");
+    const inputResultUnit = document.getElementById("inputResultUnit");
+    const quantity = document.getElementById("quantity").value;
+
+    switch(getSelected()){
+        case "km-miles":            
+            result = quantity * 0.621371;
+            inputResult.value=result.toFixed(2);
+        break;
+        case "miles-km":      
+            result = quantity * 1.60934;
+            inputResult.value=result.toFixed(2);
+        break;
+        case "foot-m":           
+            result = quantity * 0.3048;
+            inputResult.value=result.toFixed(2);
+        break;
+        case "m-foot":           
+            result = quantity * 3.28084;
+            inputResult.value=result.toFixed(2);
+        break;
+        case "cm-inch":
+            result = quantity * 0.393701;
+            inputResult.value=result.toFixed(2);
+        break;
+        case "inch-cm":
+            result = quantity * 2.54;
+            inputResult.value=result.toFixed(2);
         break;
     }
 }
 
+function exchangeUnits(){
+    const values = document.getElementById("unitsSelector");
+    const quantity = document.getElementById("quantity");
+    const result = document.getElementById("inputResult");
+    const userValue = quantity.value;
+    const resultValue = result.value;
 
-// function convert(units, n){
-//     //onchange() de input de cantidad
+    switch(getSelected()){
+        case "km-miles":            
+            values.options.selectedIndex = 1;
+            quantity.value = resultValue;
+            result.value = userValue;
+        break;
+        case "miles-km":      
+            values.options.selectedIndex = 0;
+            quantity.value = resultValue;
+            result.value = userValue;
+        break;
+        case "foot-m":           
+            values.options.selectedIndex = 3;
+            quantity.value = resultValue;
+            result.value = userValue;
+        break;
+        case "m-foot":           
+            values.options.selectedIndex = 2;
+            quantity.value = resultValue;
+            result.value = userValue;
+        break;
+        case "cm-inch":
+            values.options.selectedIndex = 5;
+            quantity.value = resultValue;
+            result.value = userValue;
+        break;
+        case "inch-cm":
+            values.options.selectedIndex = 4;
+            quantity.value = resultValue;
+            result.value = userValue;
+        break;
+    }
+    changeUnit();
+}
 
-//     switch(units){
-//         case "km-miles":
-//             result = n * 0,621371;
-//             console.log(result);
-//             inputResult.innerHTML=result;
-//         break;
-//         case "miles-km":
+//function for save conversions
+function save(){
+    //Create elements for get or modified values
+    const inputQuantity = document.getElementById("quantity");
+    const inputResult = document.getElementById("inputResult");
+    const inputUnit = document.getElementById("unit");
+    const inputResultUnit = document.getElementById("inputResultUnit");
 
-//         break;
-//         case "foot-m":
+    //Getting values from elements
+    const unit = inputUnit.value;
+    const resultUnit = inputResultUnit.value;
+    const result = inputResult.value;
+    const quantity = inputQuantity.value;
 
-//         break;
-//         case "m-foot":
+    //creating <p> for shows info
+    const p = document.createElement("p");
+    p.setAttribute("class", "infoSaved")
+    const text = document.createTextNode(quantity + " " + unit + " → " + result + " " + resultUnit);
+    p.appendChild(text);
 
-//         break;
-//         case "cm-inch":
+    //CSS for <p>
+    p.style.backgroundColor = "#acaaaa";
+    p.style.padding = "1%";
+    p.style.color = "black";
+    p.style.borderRadius = "10px";
+    p.style.width = "48%";
+    p.style.display = "inline-block"
 
-//         break;
-//         case "inch-cm":
+    const divSaved = document.getElementById("saved");
+    divSaved.appendChild(p);
 
-//         break;
-        
-//     }
+    //clearing input
+    inputQuantity.value="";
+    convert();
+    
+}
 
-// }
+
+//function for get the selected option in <select> element
+function getSelected(){
+    const units = document.getElementById("unitsSelector").options.selectedIndex;
+
+    switch(units){
+        case 0:            
+            return "km-miles"
+        break;
+        case 1:      
+            return "miles-km"
+        break;
+        case 2:           
+            return "foot-m"            
+        break;
+        case 3:           
+            return "m-foot"
+        break;
+        case 4:
+            return "cm-inch"
+        break;
+        case 5:
+            return "inch-cm"
+        break;
+    }
+}
